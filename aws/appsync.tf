8f5a54958c80325995d6c122b9b333382a5922a4
KevinArellano94 <Kevin.Arellano94@GMail.Com>
2022-10-11 12:16:06 -0400

    Added Terraform AppSync API Key.

--- aws/appsync.tf
+++ aws/appsync.tf
@@ -9,3 +9,8 @@ resource "aws_appsync_api_cache" "example" {
   type                 = "LARGE"
   ttl                  = 900
 }
+
+resource "aws_appsync_api_key" "example" {
+  api_id  = aws_appsync_graphql_api.example.id
+  expires = "2018-05-03T04:00:00Z"
+}

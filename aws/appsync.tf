6f363ed8d3eb06ca78b96607d4bd3dba272c8a1c
KevinArellano94 <Kevin.Arellano94@GMail.Com>
2022-10-12 09:05:39 -0400

    Added Terraform AppSync Data Source.

--- aws/appsync.tf
+++ aws/appsync.tf
@@ -14,3 +14,56 @@ resource "aws_appsync_api_key" "example" {
   api_id  = aws_appsync_graphql_api.example.id
   expires = "2018-05-03T04:00:00Z"
 }
+
+resource "aws_dynamodb_table" "example" {
+  name           = "example"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name = "example"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "appsync.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "example" {
+  name = "example"
+  role = aws_iam_role.example.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_dynamodb_table.example.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
